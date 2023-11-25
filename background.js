a1dd3c66aa024aab295ae232c3d0931c9e2494af
Spike433 <juhacar116@gmail.com>
2023-11-25 13:17:13 +0100

    update backgroundjs

--- background.js
+++ background.js
@@ -4,25 +4,35 @@ console.log("Event Blocker extension is starting...");
 
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
-    // Check if the request contains the specified event name
-    if (details.method === "POST" && details.requestBody) {
-      const requestBody = JSON.parse(
-        new TextDecoder().decode(details.requestBody.raw[0].bytes)
-      );
-      const eventName = "Lost focus";
-      const eventDataToBlock = "Warning - student could be cheating (alt+tab)?";
+    // Check if the request is POST and has a request body
+    if (!(details.method === "POST" && details.requestBody)) {
+      console.log("Request is not a POST request or has no request body.");
+      return;
+    }
 
-      if (
-        requestBody.entries.some(
-          (entry) =>
-            entry.eventName === eventName &&
-            entry.eventData === eventDataToBlock
-        )
-      ) {
-        // Block the request
-        console.log("Event Blocker extension blocked the request.");
-        return { cancel: true };
-      }
+    const requestBody = JSON.parse(
+      new TextDecoder().decode(details.requestBody.raw[0].bytes)
+    );
+
+    if (!(requestBody && requestBody.entries)) {
+      console.log("Request body is empty.");
+      return;
+    }
+
+    const eventName = "Lost focus";
+    const eventDataToBlock = "Warning - student could be cheating (alt+tab)?";
+
+    if (
+      requestBody.entries.some(
+        (entry) =>
+          entry.eventName === eventName && entry.eventData === eventDataToBlock
+      )
+    ) {
+      // Block the request
+      console.log("Event Blocker extension blocked the request.");
+      console.log("Raw request body: " + JSON.stringify(requestBody));
+
+      return { cancel: true };
     }
   },
   { urls: ["<all_urls>"] },
